a80f505f0e28369810c25bfc31e8bad0d3e5ca25
RANI <rani@RANIs-MacBook-Pro.local>
2025-10-30 03:35:42 +0100

    chnges

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -181,7 +181,8 @@ const Home = () => {
   return (
     <div>
       {/* Search Bar */}
-       
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1054040651597650"
+     crossorigin="anonymous"></script>
       
       {/* Visa Type Selection */}
       <Card className="mb-4">
